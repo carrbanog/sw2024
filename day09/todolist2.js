041bb052dd42bf5068cca33be613dcb5f7abc200
carrbanog <loopang5954@gmail.com>
2024-08-07 10:48:00 +0900

    ddd

--- day09/todolist2.js
+++ day09/todolist2.js
@@ -1,26 +1,15 @@
-const form = document.querySelector("form");
-const todoTxt = document.querySelector(".title");
 const saveBtn = document.querySelector("#saveBtn");
-const resultArea = document.querySelector(".resultArea");
-let todoList = [];
+const title = document.querySelector(".title");
+const form = document.querySelector("form");
 
-form.addEventListener("submit", submitEvent);
-
-function submitEvent(event){
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  todoSeq = todoList.length + 1;
-  todoList.push({
-    seq:todoSeq, title:todoTxt.value, done:false,
-  })
-  paintTodo();
-  todoTxt.value = "";
-}
-function paintTodo(){
-  resultArea.innerHTML = "";
-  todoList.forEach(todo => {
-    const li = document.createElement("li");
-    resultArea.appendChild(li);
-    li.innerHTML = todo.title;
-  })
-  // todoList.for
+  append();
+})
+
+function append(){
+  const listArea = document.querySelector(".resultArea");
+  listArea.innerHTML = `<li>${title.value}</li>`;
+  title.innerHTML = "";
+  title.focus();
 }
